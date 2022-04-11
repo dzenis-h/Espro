0e87e44124cedcff8a57b16f8864d0bd3f3d2e08
Dzenis <dzenis.hankusic@gmail.com>
2022-04-11 22:23:58 +0200

    Initial Commit

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,29 @@
+import React, { Fragment, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Footer from "../components/layout/FooterComponent";
+
+const Home = () => {
+  useEffect(() => {
+    document.title = "E S P R O"; // making the document title dynamic
+  }, []);
+
+  return (
+    <Fragment>
+      <main id="home">
+        <div className="particles">
+          <div className="parts"></div>
+        </div>
+        <div className="wrapper animate__animated animate__backInUp">
+          <h1 className="specialFont text textShadow ">
+            <Link to="./about" className="explore">
+              E x p l o r e
+            </Link>
+          </h1>
+        </div>
+      </main>
+      <Footer />
+    </Fragment>
+  );
+};
+
+export default Home;
